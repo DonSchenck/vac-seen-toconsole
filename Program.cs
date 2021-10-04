6e8d80089ff689efde7a9f0a2e039fe393a1f028
Don Schenck <don.schenck@gmail.com>
2021-10-04 11:44:23 -0400

    Back to writing to Console

--- Program.cs
+++ Program.cs
@@ -41,12 +41,12 @@ namespace vac_seen_toconsole
                     var consumeResult = consumer.Consume(token);
                     // Deserialize result
                     VaccinationEvent ve = JsonConvert.DeserializeObject<VaccinationEvent>(consumeResult.Message.Value);
-                    // Console.WriteLine("RecipientID: {0}", ve.RecipientID);
-                    // Console.WriteLine("Vaccination Type: {0}", ve.VaccinationType);
-                    // Console.WriteLine("Timestamp: {0}", ve.EventTimestamp);
-                    // Console.WriteLine("Country Code: {0}", ve.CountryCode);
-                    // Console.WriteLine("Shot Number: {0}", ve.ShotNumber);
-                    // Console.WriteLine("---------------------");
+                    Console.WriteLine("RecipientID: {0}", ve.RecipientID);
+                    Console.WriteLine("Vaccination Type: {0}", ve.VaccinationType);
+                    Console.WriteLine("Timestamp: {0}", ve.EventTimestamp);
+                    Console.WriteLine("Country Code: {0}", ve.CountryCode);
+                    Console.WriteLine("Shot Number: {0}", ve.ShotNumber);
+                    Console.WriteLine("---------------------");
                     vaxcount++;
                     if (vaxcount == 100) {
                         Console.WriteLine("{0} vaccinations counted.", vaxcount);
